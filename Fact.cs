14f248856bb9ded6dd17ee92f1df83a1fe600627
Arnþór Hjaltason <arnthorh88@gmail.com>
2020-11-02 19:19:50 +0000

    Docker images up and running

---
+++ Fact.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace trivio
+{
+    public class Fact
+    {
+        public Fact(string input) {
+            fact = input;
+        }
+        public string fact { get; set; }
+    }
+}
